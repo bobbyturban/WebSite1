7c107b89cab31bbf6c1640d1fa36e9af2c32c006
bobbyturban <41744431+bobbyturban@users.noreply.github.com>
2020-04-06 10:44:47 +0200

    Initial commit

---
+++ src/AnimateEnte.js
@@ -0,0 +1,79 @@
+﻿const name = "AnimateEnte";
+
+function animate() {
+
+
+    var loadingManager = new THREE.LoadingManager();
+    var loader = new THREE.GLTFLoader(loadingManager);
+    //var loader = new THREE.ObjectLoader(loadingManager);
+
+    var scene = new THREE.Scene();
+    var camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    scene.add(camera);
+    // Our key light is at a 3/4 angle from our subject.
+    var directionalLight = new THREE.DirectionalLight(0xffffff, .75);
+    directionalLight.position.set(-1, 1, 0);
+    scene.add(directionalLight);
+
+    // Our fill light normalizes the colors, making white 100% white.
+    var ambientLight = new THREE.AmbientLight(0xffffff, 0.25);
+    scene.add(ambientLight);
+
+    var renderer = new THREE.WebGLRenderer({ antialias: true, alpha: false });
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    renderer.domElement.style.opacity = 1;
+    document.body.appendChild(renderer.domElement);
+
+    var geometry = new THREE.BoxGeometry();
+    var material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    var cube = new THREE.Mesh(geometry, material);
+
+    //scene.add(cube);
+
+    //loader.load('Modells/Ente.gltf', function (gltf) {
+    //loader.load('Modells/AxelsSkulptur.gltf', function (gltf) {
+
+    //loader.load('AxelsSkulptur.gltf', function (gltf) {
+    //loader.setPath('Modells/');cd..
+
+    /** @type {THREE.Mesh} */
+    var ente;
+
+    loader.load("./models/Ente1.gltf",
+        //loader.load('https://threejs.org/examples/models/gltf/DamagedHelmet/glTF/DamagedHelmet.gltf',
+        function (gltf) {
+            ente = gltf.scene;
+            scene.add(gltf.scene);
+            //scene.add(gltf);
+        },
+        // called while loading is progressing
+        function (xhr) {
+
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+
+        },
+        function (error) {
+            console.error(error);
+        });
+
+    camera.position.z = 50;
+
+    function animate() {
+        requestAnimationFrame(animate);
+        ente.rotation.y += 0.01;
+        //ente.scale.multiplyScalar(Math.random());
+        if (Math.random() < 0.67) {
+            ente.scale.multiplyScalar(1.01);
+        } else {
+            ente.scale.multiplyScalar(0.99);
+        }
+
+        cube.rotation.x += 0.01;
+        cube.rotation.y += 0.01;
+
+        renderer.render(scene, camera);
+    }
+
+    animate();
+}
+export { animate };
---
+++ src/CubeMapExample.js
@@ -0,0 +1,158 @@
+﻿import * as THREE from '../libs/three/build/three.module.js';
+
+import Stats from '/libs/three/examples/jsm/libs/stats.module.js';
+
+import { OrbitControls } from '/libs/three/examples/jsm/controls/OrbitControls.js';
+import { OBJLoader } from '/libs/three/examples/jsm/loaders/OBJLoader.js';
+import { GLTFLoader } from '/libs/three/examples/jsm/loaders/GLTFLoader.js';
+
+var container, stats;
+
+var camera, scene, renderer;
+
+var pointLight;
+
+init();
+animate();
+
+function init() {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+
+    camera = new THREE.PerspectiveCamera(50, window.innerWidth / window.innerHeight, 0.1, 5000);
+    camera.position.z = 2000;
+
+    //cubemap
+    var path = '/res/textures/cube/cube/pisa/';
+    var format = '.jpg'; // works
+    //var format = '.png';    // works, but files are much bigger -> slower!
+    var urls = [
+        path + 'px' + format, path + 'nx' + format,
+        path + 'py' + format, path + 'ny' + format,
+        path + 'pz' + format, path + 'nz' + format
+    ];
+
+    var reflectionCube = new THREE.CubeTextureLoader().load(urls);
+    var refractionCube = new THREE.CubeTextureLoader().load(urls);
+    refractionCube.mapping = THREE.CubeRefractionMapping;
+
+    scene = new THREE.Scene();
+    scene.background = reflectionCube;
+
+    //lights
+    var ambient = new THREE.AmbientLight(0xffffff);
+    scene.add(ambient);
+
+    pointLight = new THREE.PointLight(0xffffff, 2);
+    scene.add(pointLight);
+
+    //materials
+    var cubeMaterial3 = new THREE.MeshLambertMaterial({ color: 0xff6600, envMap: reflectionCube, combine: THREE.MixOperation, reflectivity: 0.3 });
+    var cubeMaterial2 = new THREE.MeshLambertMaterial({ color: 0xffee00, envMap: refractionCube, refractionRatio: 0.95 });
+    var cubeMaterial1 = new THREE.MeshLambertMaterial({ color: 0xffffff, envMap: reflectionCube });
+
+    var objLoader = new OBJLoader();
+
+    objLoader.setPath('/libs/three/examples/models/obj/walt/');
+    objLoader.load('WaltHead.obj', function (object) {
+        var head = object.children[0];
+
+        head.scale.multiplyScalar(15);
+        head.position.y = - 500;
+        head.material = cubeMaterial1;
+
+        var head2 = head.clone();
+        head2.position.x = - 900;
+        head2.material = cubeMaterial2;
+
+        var head3 = head.clone();
+        head3.position.x = 900;
+        head3.material = cubeMaterial3;
+
+        //scene.add(head, head2, head3);
+        scene.add(head);
+    });
+
+    var gltfLoader = new GLTFLoader();
+    gltfLoader.setPath('/res/models/');
+
+    /** @type {THREE.Mesh} */
+    var ente;
+
+    gltfLoader.load('Ente1.gltf',
+        function (gltf) {
+            ente = gltf.scene;
+
+            ente.scale.multiplyScalar(15*4);
+            ente.position.x = -900;
+            ente.position.y = -500;
+        
+            scene.add(ente);
+        },
+        // called while loading is progressing
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    /** @type {THREE.Mesh} */
+    var artWork;
+    gltfLoader.load('AxelsSkulptur.gltf',
+        function (gltf) {
+            artWork = gltf.scene;
+
+            artWork.scale.multiplyScalar(15*3);
+            artWork.position.x = 900;
+            artWork.position.y = -500;
+            artWork.material = cubeMaterial2;
+          
+            scene.add(artWork);
+        },
+        // called while loading is progressing
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total * 100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    //renderer
+    renderer = new THREE.WebGLRenderer();
+    renderer.setPixelRatio(window.devicePixelRatio);
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    container.appendChild(renderer.domElement);
+
+    //controls
+    var controls = new OrbitControls(camera, renderer.domElement);
+    //controls.enableZoom = false;
+    //controls.enablePan = false;
+    controls.minPolarAngle = Math.PI / 4;
+    controls.maxPolarAngle = Math.PI / 1.5;
+
+    //stats
+    stats = new Stats();
+    container.appendChild(stats.dom);
+
+    window.addEventListener('resize', onWindowResize, false);
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+
+    renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function animate() {
+    requestAnimationFrame(animate);
+    render();
+}
+
+function render() {
+    renderer.render(scene, camera);
+    stats.update();
+}
